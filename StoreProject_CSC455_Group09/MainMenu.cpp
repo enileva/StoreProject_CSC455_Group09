42c19f0983c680dbe967bf2af6a295b9fddc5811
ksp829s <ksp829s@login.missouristate.edu>
2023-10-30 13:24:32 -0500

    testing

--- StoreProject_CSC455_Group09/MainMenu.cpp
+++ StoreProject_CSC455_Group09/MainMenu.cpp
@@ -2,7 +2,7 @@
 // Group 09: Aveline McEntire, Brendan Lance, Kaedon Pruiett
 /* Some notes; I've changed my mind that we should include a login.cpp. I think it makes more sense to have a file for registration to replace login.cpp
    and have users login on the store pages. This avoids confusion for an admin login, which requires a password only on the admin pages. - Aveline */
-
+// testing
 #include <iostream>
 
 // We will include headers so we can access the other cpp files.
@@ -14,25 +14,28 @@ int main()
 {
     std::string userInput;
     char menuChoice;
-    std::cout << "Welcome to the Super Rewards™ system!\n\n";
+    std::cout << "Welcome to the Super Rewardsï¿½ system!\n\n";
     std::cout << "Please choose from the following options:\n";
-    std::cout << "A. Customer registration\n"; // Registration.cpp
-    std::cout << "B. Shopping\n"; // Store.cpp
-    std::cout << "C. Redeem rewards\n"; // Store.cpp
+    std::cout << "A. Customer registration\n";    // Registration.cpp
+    std::cout << "B. Shopping\n";                 // Store.cpp
+    std::cout << "C. Redeem rewards\n";           // Store.cpp
     std::cout << "D. Product addition (admin)\n"; // Admin.cpp
-    std::cout << "E. Product removal (admin)\n"; // Admin.cpp
+    std::cout << "E. Product removal (admin)\n";  // Admin.cpp
     std::cout << "F. Customer removal (admin)\n"; // Admin.cpp
     std::cout << "G. View customers (admin)\n\n"; // Admin.cpp
 
     std::cin >> userInput;
     // Check if the input string has exactly one character
-    if (userInput.length() == 1) {
+    if (userInput.length() == 1)
+    {
         menuChoice = userInput[0];
 
         // Check if the input is a letter
-        if (isalpha(menuChoice)) {
+        if (isalpha(menuChoice))
+        {
             // switch to other cpp file with choice here.
-            switch (menuChoice) {
+            switch (menuChoice)
+            {
             case 'A':
                 handleCustomerRegistration();
                 break;
@@ -58,22 +61,23 @@ int main()
                 std::cout << "Invalid choice.\n";
             }
         }
-        else {
+        else
+        {
             std::cout << "Invalid input. Please enter a letter." << std::endl;
         }
     }
-    else {
+    else
+    {
         std::cout << "Invalid input. Please enter exactly one character." << std::endl;
     }
 
     return 0;
-
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
+// Tips for Getting Started:
 //   1. Use the Solution Explorer window to add/manage files
 //   2. Use the Team Explorer window to connect to source control
 //   3. Use the Output window to see build output and other messages
