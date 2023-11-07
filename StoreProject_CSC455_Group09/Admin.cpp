5055aa805c9b661f37ad7fa52829190208ec3156
ksp829s <ksp829s@login.missouristate.edu>
2023-11-07 13:05:13 -0600

    Admin functions partlyIthink?filesneedcreated stil

--- StoreProject_CSC455_Group09/Admin.cpp
+++ StoreProject_CSC455_Group09/Admin.cpp
@@ -1,26 +1,35 @@
 // Group 09: Aveline McEntire, Brendan Lance, Kaedon Pruiett
 #include "Admin.h"
 #include <iostream>
-void handleProductAddition() {
+
+int handleProductAddition(const std::string &ProudctAdd)
+{
     // Implementation of product addition functionality
     // Placeholder text for testing
+    products.txt.append(ProudctAdd);
     std::cout << "You have selected product addition.";
 }
 
-void handleProductRemoval() {
+int handleProductRemoval(const std::string &ProudctRemove)
+{
     // Implementation of product removal functionality
     // Placeholder text for testing
+    products.txt.remove(ProudctRemove);
     std::cout << "You have selected product removal.";
 }
 
-void handleCustomerRemoval() {
+int handleCustomerRemoval(const std::string &CustomerRemove)
+{
     // Implementation of customer removal functionality
     // Placeholder text for testing
+    customer.txt.remove(CustomerRemove);
     std::cout << "You have selected customer removal.";
 }
 
-void viewCustomers() {
+int viewCustomers(const std::string &Customer)
+{
     // Implementation of viewing customers functionality
     // Placeholder text for testing
+    customer.txt.read(Customer);
     std::cout << "You have selected view customers.";
 }
