fe9ca05442daee93fffd411e51295779a5af8bca
ksp829s <ksp829s@login.missouristate.edu>
2023-11-08 13:21:31 -0600

    product add function needs functions put in if statment to check vaild  or not

--- StoreProject_CSC455_Group09/Admin.cpp
+++ StoreProject_CSC455_Group09/Admin.cpp
@@ -6,8 +6,21 @@ int handleProductAddition(const std::string &ProudctAdd)
 {
     // Implementation of product addition functionality
     // Placeholder text for testing
-    products.txt.append(ProudctAdd);
     std::cout << "You have selected product addition.";
+    int i = 1;
+    if ( // put the product check funtions here
+    )
+    {
+        products.txt.append("product ", i, " ", ProductID, "\n");
+        products.txt.append("product ", i, " ", Name, "\n");
+        products.txt.append("product ", i, " ", Price, "\n");
+        products.txt.append("product ", i, " ", "Total items in store", TotalItems, "\n", "\n");
+        i = i + 1;
+    }
+    else
+    {
+        std::cout << "you have enterd invaild inputs\n";
+    }
 }
 
 int handleProductRemoval(const std::string &ProudctRemove)
